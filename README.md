165a814c3b1393b3b87db1e2b9db29983742f15a
redstonedesigner <redstonedesigner1@gmail.com>
2020-01-18 20:15:37 +0000

    Initial commit.

---
+++ README.md
@@ -0,0 +1,23 @@
+# Code Generator
+This project is intended to allow users to generate codes that can then be used in gift-cards, vouchers, coupons or anything else that would need a code.
+
+## Dependencies
+Install using `pip install -r requirements.txt` or, if using PipEnv, `pipenv install -r requirements.txt`
+* PyYAML - 5.1.1
+
+## Configuration
+The configuration file has two options - `format` and `charset`.
+* `charset` (String) - A list of all possible characters to be included in a code (Default: `0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ`)
+* `format` (String) - The code format to be used.  Hashtags (`#`) will be replaced with a random character from the character set (Default: `####-####-####-####`)
+
+## Usage
+To use the script, type `python app.py <amount>` where `<amount>` is the desired amount of codes.
+
+## Output
+The program will output a file, called `codes.txt` which will have all of your codes within it, each on its own line.
+To read these in to a Python program, the following line of code works best:
+```py
+with open('codes.txt','r') as file:
+  codes = file.read().splitlines()
+  file.close()
+```
---
+++ app.py
@@ -0,0 +1,74 @@
+import yaml, random, sys
+
+# Get amount from sys.argv
+try:
+    amount = int(sys.argv[1])
+except:
+    print('ERROR - Please specify an amount of codes to generate.')
+    quit()
+
+# Prepare progress bar function
+def printProgressBar (iteration, total, prefix = '', suffix = '', decimals = 1, length = 100, fill = '█', printEnd = "\r"):
+    """
+    Call in a loop to create terminal progress bar
+    @params:
+        iteration   - Required  : current iteration (Int)
+        total       - Required  : total iterations (Int)
+        prefix      - Optional  : prefix string (Str)
+        suffix      - Optional  : suffix string (Str)
+        decimals    - Optional  : positive number of decimals in percent complete (Int)
+        length      - Optional  : character length of bar (Int)
+        fill        - Optional  : bar fill character (Str)
+        printEnd    - Optional  : end character (e.g. "\r", "\r\n") (Str)
+    """
+    percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
+    filledLength = int(length * iteration // total)
+    bar = fill * filledLength + '-' * (length - filledLength)
+    print('\r%s |%s| %s%% %s' % (prefix, bar, percent, suffix), end = printEnd)
+    # Print New Line on Complete
+    if iteration == total:
+        print()
+
+# Load config
+class Config:
+    def __init__(self):
+        try:
+            # Load valid config
+            cfg = yaml.load(open('config.yml','r'),Loader=yaml.CLoader)
+            self.charset = cfg['charset']
+            self.format = cfg['format']
+        except Exception as e:
+            # Handle invalid config
+            print('ERROR - Invalid configuration file.  Regenerating...')
+            # Generate valid config
+            with open('config.yml','w') as file:
+                file.write("charset: '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'\nformat: '####-####-####-####'")
+                file.close()
+            print('ERROR - Config file regenerated.  Please take a moment to customise the configuration before running the program again.')
+            quit()
+
+config = Config()
+
+# Create output file
+with open('codes.txt','w') as file:
+    # For each code:
+    for i in range(0,amount):
+        code = ''
+        # For each character within the format:
+        for k in config.format:
+            # If character is to be randomised:
+            if k == '#':
+                # Generate random character from charset and append it to the code
+                code += str(random.choice(config.charset))
+            # If character is not to be randomised:
+            else:
+                # Append character to code
+                code += str(k)
+        # Write code to file on a new line
+        file.write(code+'\n')
+        # Advance progress bar
+        printProgressBar(i+1,amount,prefix='Generating...',suffix="("+str(i)+"/"+str(amount)+")",length=50)
+    # Close file when all codes are generated
+    file.close()
+    # Print success message
+    print('SUCCESS - Codes generated and can be found in codes.txt')
